ed4d9cabf6e4db027fbdea76347a5b0540806700
leekelleher <leekelleher@gmail.com>
2016-03-14 23:13:41 +0000

    Added (root) assets from my current (live) website
    [skip ci]

---
+++ input/_assets/503.php
@@ -0,0 +1,33 @@
+<?php
+header("HTTP/1.1 503 Service Temporarily Unavailable");
+header("Status: 503 Service Temporarily Unavailable");
+header("Retry-After: 600");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<head>
+		<title>leekelleher.com</title>
+		<meta name="robots" content="none" />
+		<style type="text/css">
+			body {background-color:#000;color:#fff;font-family:sans-serif;font-size:small;margin:0;padding:0;}
+			div#main {text-align:center;margin-top:5%;}
+			h1 {font-size:40px;letter-spacing:10px;}
+			h2 {letter-spacing:3px;}
+			a {background-color:transparent;color:#C7D82A;text-decoration:none;}
+			a:hover {text-decoration:underline;}
+		</style>
+	</head>
+	<body>
+		<div id="main">
+			<p>
+				<a href="http://www.flickr.com/photos/girliemac/6540643319/" title="503 - Service Unavailable by GirlieMac, on Flickr">
+					<img src="http://farm8.staticflickr.com/7157/6540643319_0f284d910d_o.jpg" width="750" height="600" alt="503 - Service Unavailable">
+				</a>
+			</p>
+			<h1>lee<span>kelleher</span>.com</h1>
+			<h2>is down for essential maintenance, I'll be back very soon!</h2>
+		</div>
+	</body>
+</html>
+
+
