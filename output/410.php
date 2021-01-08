3d5d26b327c5b7593ff7fc6068e71435bfcadb7a
leekelleher <leekelleher@gmail.com>
2021-01-08 14:29:50 +0000

    [WIP] Included the /output folder

---
+++ output/410.php
@@ -0,0 +1,30 @@
+<?php
+header("HTTP/1.1 410 Gone");
+header("Status: 410 Gone");
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<title>410 Gone - leekelleher.com</title>
+	<meta name="robots" content="none">
+	<style type="text/css">
+		body {background-color:#000;color:#fff;font-family:sans-serif;font-size:small;margin:0;padding:0;}
+		div#main {text-align:center;margin-top:5%;}
+		h1 {font-size:40px;letter-spacing:10px;}
+		h2 {letter-spacing:3px;}
+		a {background-color:transparent;color:#C7D82A;text-decoration:none;}
+		a:hover {text-decoration:underline;}
+	</style>
+</head>
+<body>
+	<div id="main">
+		<p>
+			<a href="https://http.cat/410" title="410 - Gone by GirlieMac, on Flickr">
+				<img src="https://http.cat/410.jpg" width="750" height="600" alt="410 - Gone">
+			</a>
+		</p>
+		<h1>lee<span>kelleher</span>.com</h1>
+		<h2>This resource is gone!</h2>
+	</div>
+</body>
+</html>
---
+++ output/503.php
@@ -0,0 +1,31 @@
+<?php
+header("HTTP/1.1 503 Service Temporarily Unavailable");
+header("Status: 503 Service Temporarily Unavailable");
+header("Retry-After: 600");
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<title>503 Service Temporarily Unavailable - leekelleher.com</title>
+	<meta name="robots" content="none">
+	<style type="text/css">
+		body {background-color:#000;color:#fff;font-family:sans-serif;font-size:small;margin:0;padding:0;}
+		div#main {text-align:center;margin-top:5%;}
+		h1 {font-size:40px;letter-spacing:10px;}
+		h2 {letter-spacing:3px;}
+		a {background-color:transparent;color:#C7D82A;text-decoration:none;}
+		a:hover {text-decoration:underline;}
+	</style>
+</head>
+<body>
+	<div id="main">
+		<p>
+			<a href="https://http.cat/503" title="503 - Service Unavailable by GirlieMac, on Flickr">
+				<img src="https://http.cat/503.jpg" width="750" height="600" alt="503 - Service Unavailable">
+			</a>
+		</p>
+		<h1>lee<span>kelleher</span>.com</h1>
+		<h2>Down for essential maintenance, I'll be back soon!</h2>
+	</div>
+</body>
+</html>
